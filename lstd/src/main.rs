314b2fde096c4d2381bd90e1f724baf58b3088ed
Yate <cmV2b2VtYWc0x3d@hotmail.com>
2026-07-24 10:44:45 +0900

    up

--- lstd/src/main.rs
+++ lstd/src/main.rs
@@ -79,4 +79,52 @@ fn main() {
         }
         _ => println!("other {:?}", xxx),
     }
+
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    println!("after {:?}", v);
+    for i in &mut v {
+        *i += 10
+    }
+    println!("befort {:?}", v);
+
+    let mut v = Vec::with_capacity(10);
+    v.extend([1, 2, 3]); // 附加数据到 v
+    println!("Vector 长度是: {}, 容量是: {}", v.len(), v.capacity());
+
+    v.reserve(100); // 调整 v 的容量，至少要有 100 的容量
+    println!(
+        "Vector（reserve） 长度是: {}, 容量是: {}",
+        v.len(),
+        v.capacity()
+    );
+
+    v.shrink_to_fit(); // 释放剩余的容量，一般情况下，不会主动去释放容量
+    println!(
+        "Vector（shrink_to_fit） 长度是: {}, 容量是: {}",
+        v.len(),
+        v.capacity()
+    );
+
+    let lt = "test";
+
+    println!(
+        "lifetime p = {:?} , r = {:?}",
+        &lt,
+        |fv: &str| -> &str { fv }(&lt)
+    );
 }
+
+pub fn test<'ta>(a: &'ta str) -> &'ta str {
+    a
+}
+
+
+// fn main() {
+//     let lt = "test";
+
+//     println!(
+//         "lifetime p = {:?} , r = {:?}",
+//         &lt,
+//         |fv: &'static str| -> &'static str { fv }(&lt)
+//     );
+// }
