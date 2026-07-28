739876f03bb00bb116378c1a1db80b79ec4bf8a6
Yate <cmV2b2VtYWc0x3d@hotmail.com>
2026-07-28 19:08:31 +0900

    s2

---
+++ timer_future/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut test1 = Test::new("test1");
+    test1.init();
+    let mut test2 = Test::new("test2");
+    test2.init();
+
+    println!("a: {}, b: {}", test1.a(), test1.b());
+    println!("a: {}, b: {}", test2.a(), test2.b());
+
+}
